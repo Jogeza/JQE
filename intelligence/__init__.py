65f2487136abcf246e59b897bdacc508433c0ef0
Jogeza <jogezamosha@gmail.com>
2026-09-01 23:32:52 +0300

    refactor(strategy): remove obsolete market state scoring stack

--- intelligence/__init__.py
+++ intelligence/__init__.py
@@ -15,8 +15,6 @@ and a few tests imported them, both updated directly).
 from intelligence.confidence_model import ConfidenceBreakdown, ConfidenceModel
 from intelligence.liquidity_engine import LiquidityEngine
 from intelligence.market_regime import detect_regime
-from intelligence.market_score import MarketScore
-from intelligence.market_state import MarketState
 from intelligence.momentum_engine import MomentumEngine
 from intelligence.trend_engine import TrendEngine
 from intelligence.volatility_engine import VolatilityEngine
@@ -25,8 +23,6 @@ __all__ = [
     "ConfidenceBreakdown",
     "ConfidenceModel",
     "LiquidityEngine",
-    "MarketScore",
-    "MarketState",
     "MomentumEngine",
     "TrendEngine",
     "VolatilityEngine",
